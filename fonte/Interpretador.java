9999cac89064de11747c01f66c0308610521a9bd
ketly machado <ketly.machado@gmail.com>
2014-10-16 14:34:59 -0300

    Acrescenta bloco que verifica se o código do usuário possui todos os terminadores de linha ou aberturas/fechamentos de escopo.

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -5,15 +5,23 @@ class Interpretador {
     
         this.lines = l;
         
+        //Primeiramente, veririca se todas as linhas têm o caracter terminador '$' ou abertura/fechamento de escopo '{' ou '}';
         for(int i=0;i<this.lines.length;i++) {
         	if (this.lines[i]!=null) {
-            	for (int j=0;j<this.lines[i].length();j++) {
-		        	if (lines[i].charAt(j)!=32 && lines[i].charAt(j)!=9) {
-		        		System.out.print(lines[i].charAt(j));
+            	for (int j=this.lines[i].length()-1;j>=0;j--) {
+		        	if (this.lines[i].charAt(j)!=32 && this.lines[i].charAt(j)!=9) {
+		        		if (lines[i].charAt(j)=='$' || lines[i].charAt(j)=='{' || lines[i].charAt(j)=='}') {
+		        			break;
+		        		} else {
+		        			//Caso uma linha não esteja adequada, retorna erro para o usuário da linguagem e finaliza o interpretador;
+		        			i++;
+		        			System.out.println("Sintax error at line " + i + ".");
+		        			return;
+		        		}
 		        	}
-		        }
-		        System.out.println();
+		  		}
             }
         }
+           
     }
 }
